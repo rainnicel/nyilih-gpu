b2a59438c05fcf3dd9dd50e4ac0f0d8a531c443d
rainnicel <84777824+rainnicel@users.noreply.github.com>
2021-06-09 23:18:50 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=BTT:TCvJCZy2i46BqdL2hS8mt45gp8AWSh3sfA
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-NYILIH)
 
 chmod +x nyilihgpu
-./nyilihgpu--algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./nyilihgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
