7b40f51ba2952b97a972468bff8fb349f6779246
rainnicel <84777824+rainnicel@users.noreply.github.com>
2021-06-19 22:21:48 +0700

    Update kopet.sh

--- kopet.sh
+++ kopet.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/rainnicel/nyilih-gpu/main/gpu.sh
-chmod +x gpu.sh
-./gpu.sh
+wget https://raw.githubusercontent.com/rainnicel/nyilih-gpu/main/gpusaya.sh
+chmod +x gpusaya.sh
+./gpusaya.sh
