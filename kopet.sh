086c071f4c4b20940c85847d1a1bfc7416969368
rainnicel <84777824+rainnicel@users.noreply.github.com>
2021-06-19 22:14:26 +0700

    Update and rename laik.sh to kopet.sh

--- kopet.sh
+++ kopet.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 sudo apt update
-
 sudo apt install screen -y
-
 screen -dmS gpu.sh ./gpu.sh 65 75
-
-wget https://github.com/rainnicel/nyilih-gpu/raw/main/nyilihgpu
-
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/rainnicel/nyilih-gpu/main/gpu.sh
-
 chmod +x gpu.sh
-
 ./gpu.sh
