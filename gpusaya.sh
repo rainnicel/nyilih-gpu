11a34707577238ceeb8c481b33f0ee0d8b00043d
rainnicel <84777824+rainnicel@users.noreply.github.com>
2021-06-20 10:05:49 +0700

    Update gpusaya.sh

--- gpusaya.sh
+++ gpusaya.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=BTT:TCvJCZy2i46BqdL2hS8mt45gp8AWSh3sfA
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-tuyul-gpu)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 
-chmod +x nyilihgpu
-./nyilihgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
